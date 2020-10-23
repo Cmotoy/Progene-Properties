12e76562394bd5e7b9347c207869f21f4c53c26a
Oluwabunmi Omotoyinbo <cmotoy@outlook.com>
2020-10-23 15:43:12 +0100

    Added Columns to Property Entities

--- src/ProgenesisProperties.data/Migrations/ApplicationDb/20201023143416_Added columns to properties entity.Designer.cs
+++ src/ProgenesisProperties.data/Migrations/ApplicationDb/20201023143416_Added columns to properties entity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProgenesisProperties.data.DatabaseContext.ApplicationDbContext;
 
 namespace ProgenesisProperties.data.Migrations.ApplicationDb
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201023143416_Added columns to properties entity")]
+    partial class Addedcolumnstopropertiesentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
